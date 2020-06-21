607c4d819ffdff08a4d7c45d30b6b57d4cdeb180
ignatandrei <ignatandrei@yahoo.com>
2020-06-22 00:19:23 +0300

    first version jaeger

---
+++ src/JaegerNetCore/TestConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var activity = new Activity("test");
+            //TODO: add jaeger, open telemetry, others
+            /*
+             * while($true){
+$x= docker run -d --name jaeger   -e COLLECTOR_ZIPKIN_HTTP_PORT=9411   -p 5775:5775/udp   -p 6831:6831/udp   -p 6832:6832/udp   -p 5778:5778   -p 16686:16686  -p 14268:14268  -p 14250:14250  -p 9411:9411   jaegertracing/all-in-one:1.18
+Write-Host 'asasd' $x
+echo http://localhost:16686
+pause
+docker container rm $x -f
+}
+             */
+        }
+    }
+}
