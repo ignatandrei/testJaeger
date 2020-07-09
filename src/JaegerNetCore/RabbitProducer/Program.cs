80146fa326485dc2ca4995d4735ef947447489e6
ignatandrei <ignatandrei@yahoo.com>
2020-07-09 20:52:02 +0300

    add activity

--- src/JaegerNetCore/RabbitProducer/Program.cs
+++ src/JaegerNetCore/RabbitProducer/Program.cs
@@ -1,12 +1,46 @@
 ﻿using RabbitMQ.Client;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace RabbitProducer
 {
     class Program
     {
+        static private Activity GetNewActionFromCurrent(
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string sourceFilePath = "",
+            [CallerLineNumber] int sourceLineNumber = 0)
+        {
+            var curent = Activity.Current;
+
+            var traceId = curent.TraceId;
+            var spanId = curent.SpanId;
+            if (curent?.Baggage.Count(it => it.Key == "MyTraceId") > 0)
+            {
+
+                traceId = ActivityTraceId.CreateFromString(curent.GetBaggageItem("MyTraceId"));
+                spanId = ActivitySpanId.CreateFromString(curent.GetBaggageItem("MySpanId"));
+            }
+
+            var activity = new Activity(memberName)
+                .SetParentId(traceId, spanId)
+                .Start();
+
+            activity.AddBaggage("MyTraceId", activity.TraceId.ToHexString());
+            activity.AddBaggage("MySpanId", activity.SpanId.ToHexString());
+            activity.AddTag("CallerMemberName", memberName);
+            activity.AddTag("CallerFilePath", sourceFilePath);
+            activity.AddTag("CallerLineNumber", sourceLineNumber.ToString());
+
+
+            return activity;
+
+
+        }
         static void Main(string[] args)
         {
             var factory = new ConnectionFactory() {
@@ -27,8 +61,11 @@ namespace RabbitProducer
                     string message = "Message sent at :"+ DateTime.Now.ToString("o");
                     var body = Encoding.UTF8.GetBytes(message);
                     var props = model.CreateBasicProperties();
+                    var act = GetNewActionFromCurrent();
                     props.Headers = new Dictionary<string, object>();
-                    props.Headers.Add("andrei", "test");
+                    props.Headers.Add("MyTraceId", act.TraceId);
+                    props.Headers.Add("MySpanId", act.TraceId);
+
                     props.Persistent = true;
 
                     model.BasicPublish(exchange: "",
