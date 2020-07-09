d82988bc9ec597289650f1648aada87cbe591f90
ignatandrei <ignatandrei@yahoo.com>
2020-07-09 20:49:05 +0300

    rabbit

---
+++ src/JaegerNetCore/RabbitConsumer/Program.cs
@@ -0,0 +1,50 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace RabbitConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "user",
+                Password = "password"
+            };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body.ToArray();
+                        var props = ea.BasicProperties.Headers;
+                        var f = props.FirstOrDefault();
+                        Console.WriteLine(f.Key + f.Value);
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] Received {0}", message);
+                    };
+                    channel.BasicConsume(queue: "hello",
+                                         autoAck: true,
+                                         consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+
+                }
+            }
+        }
+    }
+}
---
+++ src/JaegerNetCore/RabbitProducer/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitProducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() {
+                HostName = "localhost",
+                UserName ="user",
+                Password="password"
+            };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var model = connection.CreateModel())
+                {
+                    model.QueueDeclare(queue: "hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+                    string message = "Message sent at :"+ DateTime.Now.ToString("o");
+                    var body = Encoding.UTF8.GetBytes(message);
+                    var props = model.CreateBasicProperties();
+                    props.Headers = new Dictionary<string, object>();
+                    props.Headers.Add("andrei", "test");
+                    props.Persistent = true;
+
+                    model.BasicPublish(exchange: "",
+                                         routingKey: "hello",
+                                         basicProperties: props,
+                                         body: body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+            }
+        }
+    }
+}
